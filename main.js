e85db7c06f97b5f6bd123b03066b5fb6b73fe302
Roberto E. Riquelme Sáez <rers.92@gmail.com>
2023-01-27 11:10:24 -0300

    wave 4 creeps

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const spawn_name = "Spawn1";
 var wave_creep_1 = [WORK,CARRY,MOVE,MOVE]; // 250
 var wave_creep_2 = [WORK,WORK,WORK,CARRY,MOVE,MOVE,MOVE,MOVE]; // 550
 var wave_creep_3 = [WORK,WORK,WORK,WORK,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE]; // 800
+var wave_creep_4 = [WORK,WORK,WORK,WORK,WORK,WORK,CARRY,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE]; // 1300
 var basic_creep = [WORK,CARRY,MOVE];// 200
 var basic_miner = [WORK,WORK,MOVE,MOVE];//300
 
@@ -15,6 +16,7 @@ module.exports.loop = function () {
         Game.spawns[spawn_name].spawnCreep(wave_creep_1, "wave_1"+Game.time.toString().slice(-2), {memory: {role: "wave_1", perma_role: "none"}});
     }else if (current_lvl >= 3 && _.filter(Game.creeps).length < 8){
         console.log("need third wave");
+        Game.spawns[spawn_name].spawnCreep(wave_creep_4, "wave_4"+Game.time.toString().slice(-2), {memory: {role: "wave_1", perma_role: "none"}});
         Game.spawns[spawn_name].spawnCreep(wave_creep_3, "wave_3"+Game.time.toString().slice(-2), {memory: {role: "wave_1", perma_role: "none"}});
         //Game.spawns[spawn_name].spawnCreep(wave_creep_1, "wave_1"+Game.time.toString().slice(-2), {memory: {role: "wave_1", perma_role: "none"}});
         //Game.spawns[spawn_name].spawnCreep(wave_creep_2, "wave_2"+Game.time.toString().slice(-2), {memory: {role: "wave_1", perma_role: "none"}});
