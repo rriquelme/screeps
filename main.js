2eeb08adc968e92972d750b5308188afd964d0ed
Roberto E. Riquelme Sáez <rers.92@gmail.com>
2023-02-06 11:43:44 -0300

    minor fix for lvl 5

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var wave_creep_1 = [WORK,CARRY,MOVE,MOVE]; // 250
 var wave_creep_2 = [WORK,WORK,WORK,CARRY,MOVE,MOVE,MOVE,MOVE]; // 550
 var wave_creep_3 = [WORK,WORK,WORK,WORK,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE]; // 800
 var wave_creep_4 = [WORK,WORK,WORK,WORK,WORK,WORK,CARRY,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE]; // 1300
+var wave_creep_5 = [WORK,WORK,WORK,WORK,WORK,WORK,WORK,WORK,CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE]; //1800
 var basic_creep = [WORK,CARRY,MOVE];// 200
 var basic_miner = [WORK,WORK,MOVE,MOVE];//300
 
@@ -28,7 +29,7 @@ module.exports.loop = function () {
         console.log("need first wave");
         Game.spawns[spawn_name].spawnCreep(wave_creep_2, "wave_2_"+Game.time.toString().slice(-2), {memory: {role: "wave_1", perma_role: "none"}});
         Game.spawns[spawn_name].spawnCreep(wave_creep_1, "wave_1_"+Game.time.toString().slice(-2), {memory: {role: "wave_1", perma_role: "none"}});
-    }else if (current_lvl >= 3 && _count_creeps < 8){
+    }else if (current_lvl >= 3 && current_lvl < 5 && _count_creeps < 8){
         console.log("need fourth wave");
         Game.spawns[spawn_name].spawnCreep(wave_creep_4, "wave_4_"+Game.time.toString().slice(-2), {memory: {role: "wave_1", perma_role: "none"}});
         //Game.spawns[spawn_name].spawnCreep(wave_creep_1, "wave_1_"+Game.time.toString().slice(-2), {memory: {role: "wave_1", perma_role: "none"}});
@@ -38,12 +39,18 @@ module.exports.loop = function () {
             Game.spawns[spawn_name].spawnCreep(wave_creep_2, "wave_2_"+Game.time.toString().slice(-2), {memory: {role: "wave_1", perma_role: "none"}});
             Game.spawns[spawn_name].spawnCreep(wave_creep_1, "wave_1_"+Game.time.toString().slice(-2), {memory: {role: "wave_1", perma_role: "none"}});
         }
-        //Game.spawns[spawn_name].spawnCreep(wave_creep_2, "wave_2"+Game.time.toString().slice(-2), {memory: {role: "wave_1", perma_role: "none"}});
-        
-        //Game.spawns[spawn_name].spawnCreep(wave_creep_1, "wave_1"+Game.time.toString().slice(-2), {memory: {role: "wave_1", perma_role: "none"}});
-
+    } else if (current_lvl >= 5 && _count_creeps < 8){
+        console.log("need fifth wave");
+        Game.spawns[spawn_name].spawnCreep(wave_creep_5, "wave_5_"+Game.time.toString().slice(-2), {memory: {role: "wave_1", perma_role: "none"}});
+        if (_count_creeps < 6 && _count_creeps >= 3){
+            Game.spawns[spawn_name].spawnCreep(wave_creep_4, "wave_4_"+Game.time.toString().slice(-2), {memory: {role: "wave_1", perma_role: "none"}});
+        } else  if (_count_creeps < 3){
+            Game.spawns[spawn_name].spawnCreep(wave_creep_3, "wave_3_"+Game.time.toString().slice(-2), {memory: {role: "wave_1", perma_role: "none"}});
+            Game.spawns[spawn_name].spawnCreep(wave_creep_1, "wave_1_"+Game.time.toString().slice(-2), {memory: {role: "wave_1", perma_role: "none"}});
+        }
 
     }
+
     //var wave_creeps = _.filter(Game.creeps, (creep) => creep.memory.role == "wave_1").length;
 
     for(var name in Game.creeps) {
